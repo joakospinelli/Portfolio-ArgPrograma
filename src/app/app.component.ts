85a60cdf46efb0848f31b30d3bdc47396f3e8b70
Joaquin Spinelli <joakospinelli@gmail.com>
2022-07-03 23:18:31 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'portfolio-argprograma';
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    let current: any = "";
+    document.querySelectorAll('section').forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+        if (section.getAttribute("id") != null){
+          current = section.getAttribute("id");
+          console.log(current);
+        }
+      }
+    });
+    
+    document.querySelectorAll('.nb').forEach((nb) => {
+      nb.classList.remove('active');
+      if (nb.classList.contains(current)){
+        nb.classList.add('active');
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { Error404Component } from './components/error404/error404.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { LoginComponent } from './components/login/login.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactoComponent,
+    EducacionComponent,
+    Error404Component,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    LoginComponent,
+    PortfolioComponent,
+    ProyectosComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
